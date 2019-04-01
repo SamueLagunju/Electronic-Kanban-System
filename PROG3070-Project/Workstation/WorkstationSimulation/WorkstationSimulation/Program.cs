626f8f7b881f36ca43b7c7efd8accdea71c2b9ec
Oloruntoba Lagunju <Olagunju9921@conestogac.on.ca>
2019-04-01 18:32:37 -0400

    Workstation Simulation solution can create a workstation but stored procedure functionality has not been implemented.

--- PROG3070-Project/Workstation/WorkstationSimulation/WorkstationSimulation/Program.cs
+++ PROG3070-Project/Workstation/WorkstationSimulation/WorkstationSimulation/Program.cs
@@ -2,20 +2,151 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace WorkstationSimulation
 {
     class Program
     {
+        static string connString = "Server= localhost; Initial Catalog=Kanban System Data; Integrated Security=SSPI;";
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            bool runningStatus = true;
+            int workStationIDBuffer = 0; string experienceBuffer = ""; int experienceLevel = 0;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //If the connection to the database failed, close the application.
+            if (!TestConnection()) { Environment.Exit(0); }
+
+            //While loop to keep running until the user's input has been determined valid
+            while(runningStatus)
+            {
+                try
+                {
+                    Console.Write("Please input StationID: ");
+                    workStationIDBuffer = Convert.ToInt32(Console.ReadLine());
+
+                    while(runningStatus)
+                    {
+
+                        Console.WriteLine("1. New/Rookie");
+                        Console.WriteLine("2. Experienced");
+                        Console.WriteLine("3. Very Experienced");
+                        Console.Write("Pick one of the experiences: ");
+                        int userChoice = Convert.ToInt32(Console.ReadLine());
+
+                        //Switch statement to set the experienceBuffer to the user's input
+                        switch(userChoice)
+                        {
+                            case 1:
+                                experienceBuffer = "New/Rookie";
+                                experienceLevel = 1;
+                                runningStatus = false;
+                                break;
+                            case 2:
+                                experienceBuffer = "Experienced";
+                                experienceLevel = 2;
+                                runningStatus = false;
+                                break;
+                            case 3:
+                                experienceBuffer = "Very Experienced";
+                                experienceLevel = 3;
+                                runningStatus = false;
+                                break;
+                            default:
+                                Console.WriteLine("Invalid Input!");
+                                break;
+                        }
+                    }
+
+                }
+                catch (Exception ExceptionError) { Console.WriteLine(ExceptionError.Message); ; }
+            }
+
+            WorkStation newStation = new WorkStation(workStationIDBuffer, experienceBuffer, experienceLevel); runningStatus = true;
+
+            while(runningStatus)
+            {
+                if(!RunInsertStoredProcedure(newStation))
+                {
+                    try
+                    {
+                        Console.Write("Please input a new StationID: ");
+                        newStation.SetWorkStationID(Convert.ToInt32(Console.ReadLine()));
+                    }
+                    catch(Exception exceptError)
+                    {
+                        Console.WriteLine(exceptError.Message);
+                    }
+                }
+
+                else { runningStatus = false; }
+            }
+
+            runningStatus = true;
+
+            while(runningStatus)
+            {
+                Console.Write("Press 1 to start the simulation: ");
+            }
+            
+
+
+        }
+
+        /*
+        * Function		:   TestConnection()
+        * Description	:   Checks the connection between the form and SQL database.
+        * Parameters	:   N/A
+        * Returns		:   status - Bool value that indicates if it was successful/unsucessful
+        */
+        static bool TestConnection()
+        {
+            bool status;       
+            SqlConnection sqlConnToServer = new SqlConnection(connString);   //Creatinng a connectiong
+            try
+            {
+                sqlConnToServer.Open();  //Opening the connection to the database
+                status = true;  //If no exception is thrown, set status to true
+                sqlConnToServer.Close(); //Close the connection
+            }
+            //If any exception was thrown, set status to false
+            catch (Exception exceptError)
+            {
+                Console.WriteLine(exceptError.Message);
+                status = false;
+            }
+
+            return status;
+        }
+
+        static bool RunInsertStoredProcedure(WorkStation workStation)
+        {
+            bool status = true;
+
+            using (SqlConnection sqlConnection = new SqlConnection(connString))
+            {
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand("InstantiateStation", sqlConnection);
+                sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("@StationNumber", workStation.GetWorkStationID());
+                sqlCommand.Parameters.AddWithValue("@Experience", workStation.GetWorkStationExperience());
+                sqlCommand.Parameters.AddWithValue("@DefectRate", workStation.GetWorkStationDefectRate());
+                sqlCommand.Parameters.AddWithValue("@Speed", workStation.GetWorkStationSpeed());
+
+
+                var returnParameter = sqlCommand.Parameters.Add("@result", SqlDbType.Int);
+                returnParameter.Direction = ParameterDirection.ReturnValue;
+
+                sqlCommand.ExecuteNonQuery();
+                int queryResult = (int) returnParameter.Value;
+                if (!(queryResult > 0)) { status = false; }
+            }
+
+            return status;
         }
     }
 }
---
+++ PROG3070-Project/Workstation/WorkstationSimulation/WorkstationSimulation/WorkStation.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WorkstationSimulation
+{
+    class WorkStation
+    {
+        private int workStationID;
+        private string experience;
+        private float defectRate;
+        private float speed;
+
+        public WorkStation(int stationIDBuffer, string experienceBuffer, int experienceLevel)
+        {
+            this.workStationID = stationIDBuffer;
+            switch(experienceLevel)
+            {
+                case 1:
+                    this.experience = experienceBuffer;
+                    this.defectRate = (float)0.85;
+                    this.speed = (float)(GetTimeScale() * 1.5 * GenerateRandomNumber(0.9, 1.1));
+                    break;
+                case 2:
+                    this.experience = experienceBuffer;
+                    this.defectRate = (float)0.5;
+                    this.speed = (float)(GetTimeScale() * GenerateRandomNumber(0.9, 1.1));
+                    break;
+                case 3:
+                    this.experience = experienceBuffer;
+                    this.defectRate = (float)0.15;
+                    this.speed = (float)(GetTimeScale() * 0.85 *GenerateRandomNumber(0.9, 1.1));
+                    break;
+            }
+        }
+
+        /*
+        * Function		:   GetTimeScale()
+        * Description	:   Acquires the Time Scale value from the Configuration tableS
+        * Parameters	:   N/A
+        * Returns		:   TimeScaleBuffer - int
+        */
+        public int GetTimeScale()
+        {
+
+            string connString = "Server= localhost; Initial Catalog=Kanban System Data; Integrated Security=SSPI;"; //Connection string to connect to the database
+            string commandString = "SELECT [Value] FROM [Configuration] WHERE [ITEM] = 'TimeScale'" ;   //Query string 
+            int timeScaleBuffer;    //Value to be assigned from the database query
+
+            using (SqlConnection tempConnection = new SqlConnection(connString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(commandString, tempConnection);
+
+                try
+                {
+                    tempConnection.Open();
+                    timeScaleBuffer = (int)sqlCommand.ExecuteScalar();  
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    timeScaleBuffer = 60;   //Default value from the requirements page
+                }
+            }
+
+            return timeScaleBuffer;
+        }
+
+        //From https://stackoverflow.com/questions/17786771/random-double-between-given-numbers
+        public double GenerateRandomNumber(double minValue, double maxValue)
+        {
+            Random randomNumer = new Random();
+            double randomValue = randomNumer.NextDouble();
+
+            return minValue + (randomValue * (maxValue - minValue));
+        }
+
+        //Getters
+        public int GetWorkStationID() { return this.workStationID; }
+        public string GetWorkStationExperience() { return this.experience; }
+        public float GetWorkStationDefectRate() { return this.defectRate; }
+        public float GetWorkStationSpeed() { return this.speed; }
+
+        //Setters
+        public void SetWorkStationID(int newStationID) { this.workStationID = newStationID; }
+        public void SetWorkStationExperience(string newStationExperience) { this.experience = newStationExperience; }
+        public void SetWorkStationDefectRate(float newDefectRate) { this.defectRate = newDefectRate; }
+        public void SetWorkStationSpeed(float newStationSpeed) { this.speed = newStationSpeed; }
+    }
+}
